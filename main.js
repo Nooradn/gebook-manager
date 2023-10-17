19140c1188e8e3e05494799099be8071399f0933
Noor Adn <muhnooradnassaid@gmail.com>
2023-10-17 21:40:34 +0700

    reconfigure button

--- main.js
+++ main.js
@@ -1,78 +1,101 @@
 // Main JS
 
-// book data
-const unreadShelf = [];
-const readedShelf = []
+// book array data
+const nowReadingShelf = [];
+const hasReadedShelf = [];
 
-// 1st func after all html elements loaded 
+// book html container
+const nowReadingContainer = document.getElementById("now-book-reading");
+const hasReadedContainer = document.getElementById("readed-book-almanac");
+
+// -----------------------------------------------
+
+// start from here
 document.addEventListener("DOMContentLoaded", function () {
   const inputBook = document.getElementById("input-book");
   inputBook.addEventListener("submit", function (e) {
     e.preventDefault();
-    onSubmitBook();
+    addNewBook();
   });
 });
 
+// -----------------------------------------------
 
-// submit book button
-function onSubmitBook() {
+// submit book button - add new book
+function addNewBook() {
   const id = +new Date();
   const title = document.getElementById("input-book-title").value;
   const author = document.getElementById("input-book-author").value;
   const year = document.getElementById("input-book-year").value;
   const isComplete = document.getElementById("book-completed-check").checked;
 
+  // prepare data
   const bookData = {
-    id, //num
-    title, //str
-    author, //str
-    year, //num
-    isComplete, //bool
+    id,
+    title,
+    author,
+    year,
+    isComplete,
   };
 
-  const containerReading = document.getElementById("now-book-reading");
-  const containerReaded = document.getElementById("readed-book-almanac");
+  // push data to relevant shelves
+  if (!isComplete) {
+    nowReadingShelf.push(bookData);
+  } else {
+    hasReadedShelf.push(bookData);
+  }
 
+  createBookElement(bookData);
+
+  console.log({ nowReadingShelf, hasReadedShelf });
+  return bookData;
+}
+
+// -----------------------------------------------
+
+// create book html element
+function createBookElement(item) {
   const containerAddedBook = document.createElement("article");
   containerAddedBook.classList.add("container-book-item");
 
-  const addedBook = document.createElement("div");
-  addedBook.innerHTML = `
-    <h3>${title}</h3>
-    <p>${author} - ${year}</p>`;
+  // create book desc
+  const bookDescElement = document.createElement("div");
+  bookDescElement.innerHTML = `
+    <h3>${item.title}</h3>
+    <p>${item.author} - ${item.year}</p>`;
 
-  const makeActionButton = document.createElement("div");
-  makeActionButton.classList.add("container-action");
+  // button thingy
+  const actionButtons = document.createElement("div");
+  actionButtons.classList.add("container-action");
 
-  // REVISI, dibuat individual ae ges
-  if (!isComplete) {
-    makeActionButton.innerHTML = `
-    <button class='mark-readed-act'>Mark as Completed</button>
-    <button class='delete-act'>Delete Book</button>`;
+  const markReadBtn = document.createElement("button");
+  markReadBtn.classList.add("mark-readed-act");
+  markReadBtn.innerText = "Mark Read";
+
+  const markUnreadBtn = document.createElement("button");
+  markUnreadBtn.classList.add("mark-unread-act");
+  markUnreadBtn.innerText = "Mark Unread";
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("delete-act");
+  deleteBtn.innerText = "Delete Book";
+
+  if (!item.isComplete) {
+    actionButtons.appendChild(markReadBtn);
+    actionButtons.appendChild(deleteBtn);
   } else {
-    makeActionButton.innerHTML = `
-    <button class="mark-unread-act">Unread</button>
-    <button class='delete-act'>Delete Book</button>`;
+    actionButtons.appendChild(markUnreadBtn);
+    actionButtons.appendChild(deleteBtn);
   }
 
-  containerAddedBook.append(
-    addedBook,
-    makeActionButton
-  );
+  containerAddedBook.appendChild(bookDescElement);
+  containerAddedBook.appendChild(actionButtons);
 
-  if (!isComplete) {
-    containerReading.prepend(containerAddedBook);
-    unreadShelf.push(bookData);
+  if (!item.isComplete) {
+    nowReadingContainer.prepend(containerAddedBook);
+    nowReadingShelf.push(item);
   } else {
-    containerReaded.prepend(containerAddedBook);
-    readedShelf.push(bookData);
+    hasReadedContainer.prepend(containerAddedBook);
+    hasReadedShelf.push(item);
   }
-
-
-
-  console.log({unreadShelf, readedShelf});
-  return bookData;
-  
 }
-
-
---
+++ pseudo.md
@@ -0,0 +1,8 @@
+
+# Add New Book
+- user fill data to form
+- clicked the button
+- trigger function addNewBook
+  - get the id, title, author, year, and isCompleted
+  - prepare and push that data into relevant array
+  - generate html element
